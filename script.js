76938ed870a926f678804d58743fd22b1565ebdf
Violette <mori1819814@lcsd2kids.org>
2021-05-10 09:31:07 -0600

    finished

--- script.js
+++ script.js
@@ -7,8 +7,10 @@ function createGrid(x) {
         for (let columns = 0; columns < x; columns++) {
             let cell = document.createElement("div");
             cell.classList.add("grid");
-            cell.textContent = ":)";
             cell.style.color = "blue";
+            cell.addEventListener("mouseover", function ( event ) {
+                event.target.style.backgroundColor = "rgb(211, 114, 211)";
+            });
             row.appendChild(cell);
         };
         container.appendChild(row);
@@ -18,9 +20,6 @@ function createGrid(x) {
 
 createGrid(16);
 
-container.addEventListener("mouseover", function ( event ) {
-    event.target.style.color = "black";
-});
 
 
 
